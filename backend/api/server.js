12b18ee01d337647c684323b930bd77e7ff0160f
SUGATACHANDA <scbabai2704@gmail.com>
2025-12-23 12:35:50 +0530

    refactor: comment out seedData call in server.js and update mongoose settings in db.js

--- backend/api/server.js
+++ backend/api/server.js
@@ -37,7 +37,7 @@ const studentRoutes = require("../routes/studentRoutes");
 
 const startServer = async () => {
     await connectDB();
-    await seedData();
+    // await seedData();
 };
 startServer();
 
--- backend/config/db.js
+++ backend/config/db.js
@@ -1,11 +1,16 @@
 // config/db.js
 const mongoose = require('mongoose');
 
+mongoose.set("bufferCommands", false);
+mongoose.set("strictQuery", true);
+
 const connectDB = async () => {
     try {
         const conn = await mongoose.connect(process.env.MONGO_URI, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
+            keepAliveInitialDelay: 300000000,
+            keepAlive: true,
         });
         console.log(`MongoDB Connected: ${conn.connection.host}`);
     } catch (error) {
